9415464b302137cc809dd552031d9f5bd595b7d2
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-08-29 22:22:57 +0600

    event & EventEmitter

---
+++ index.js
@@ -0,0 +1,9 @@
+const School = require("./school");
+const school = new School();
+
+//register a listener for bellRing event
+school.on("bellRing", ({ period, text }) => {
+  console.log(`we need to run because ${period} ${text}`);
+});
+
+school.startPeriod();
